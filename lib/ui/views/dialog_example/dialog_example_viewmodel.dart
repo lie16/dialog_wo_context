45d271bd08ef9a4a351b131d61e4531f3201be82
lie16 <yonattan_hope@yahoo.com>
2020-11-09 10:54:55 +0800

    add showBasicDialog

--- lib/ui/views/dialog_example/dialog_example_viewmodel.dart
+++ lib/ui/views/dialog_example/dialog_example_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:dialog_wo_context/app/locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -7,8 +8,20 @@ class DialogExampleViewModel extends BaseViewModel {
 
   DialogResponse _dialogResponse;
   DialogResponse get customDialogResult => _dialogResponse;
+  final _dialogService = locator<DialogService>();
 
-  Future showBasicDialog() async {}
+  Future showBasicDialog() async {
+    DialogResponse response = await _dialogService.showDialog(
+      title: 'The Basic Dialog',
+      description:
+          'This is the description for the dialog that shows up under the title',
+      buttonTitle: 'This is the main button title',
+      dialogPlatform: DialogPlatform.Cupertino, // DialogPlatform.Material
+      cancelTitle: 'Cancel',
+    );
+
+    print('DialogResponse: ${response?.confirmed}');
+  }
 
   Future showConfirmationDialog() async {}
 
