0ae936f2ce022e29cca21b4e76b9f8722fe1578c
lie16 <yonattan_hope@yahoo.com>
2020-11-09 11:24:59 +0800

    add _customDialogUi

--- lib/ui/setup_dialog_ui.dart
+++ lib/ui/setup_dialog_ui.dart
@@ -1,61 +1,96 @@
 import 'package:dialog_wo_context/app/locator.dart';
+import 'package:dialog_wo_context/enums/dialog_type.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 void setupDialogUi() {
   var dialogService = locator<DialogService>();
 
-
   dialogService.registerCustomDialogBuilder(
     //TODO variant perlu dicari tahu, pleae refer to pub.dev
-      variant: null,
-      builder: (context, dialogRequest) => Dialog(
+    variant: null,
+    builder: (context, dialogRequest) => Dialog(
+      child: _customDialogUi(
+        dialogRequest,
+        (dialogResponse) => dialogService.completeDialog(dialogResponse),
+      ),
+    ),
+  );
+}
+
+Widget _customDialogUi(
+  DialogRequest dialogRequest,
+  Function(DialogResponse) onDialogTap,
+) {
+  var dialogType = dialogRequest.customData as DialogType;
+  switch (dialogType) {
+    case DialogType.Basic:
+    default:
+      return _BasicCustomDialog(
+        dialogRequest: dialogRequest,
+        onDialogTap: onDialogTap,
+      );
+  }
+}
+
+class _BasicCustomDialog extends StatelessWidget {
+  final DialogRequest dialogRequest;
+  final Function(DialogResponse) onDialogTap;
+  const _BasicCustomDialog({
+    Key key,
+    this.dialogRequest,
+    this.onDialogTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            dialogRequest.title,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            dialogRequest.description,
+            style: TextStyle(
+              fontSize: 18,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          GestureDetector(
+            // Complete the dialog when you're done with it to return some data
+            onTap: () => onDialogTap(DialogResponse(confirmed: true)),
             child: Container(
-              padding: const EdgeInsets.all(20),
+              child: dialogRequest.showIconInMainButton
+                  ? Icon(Icons.check_circle)
+                  : Text(dialogRequest.mainButtonTitle),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              width: double.infinity,
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    dialogRequest.title,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    dialogRequest.description,
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  GestureDetector(
-                    // Complete the dialog when you're done with it to return some data
-                    onTap: () => dialogService
-                        .completeDialog(DialogResponse(confirmed: true)),
-                    child: Container(
-                      child: dialogRequest.showIconInMainButton
-                          ? Icon(Icons.check_circle)
-                          : Text(dialogRequest.mainButtonTitle),
-                      alignment: Alignment.center,
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.redAccent,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  )
-                ],
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(5),
               ),
             ),
-          ));
+          )
+        ],
+      ),
+    );
+  }
 }
+
+
